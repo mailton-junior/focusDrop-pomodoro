32256afa6979c1762296d2e47bc700feaf2b3468
Mailton Junior <mailtonjr7@gmail.com>
2025-07-14 17:53:48 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
       <Container>
-        <section>LOGO</section>
+        <Heading>LOGO</Heading>
       </Container>
 
       <Container>
-        <section>MENU</section>
-      </Container>
-
-      <Container>
-        <section>FORM</section>
-      </Container>
-
-      <Container>
-        <section>FOOTER</section>
+        <Heading>MENU</Heading>
       </Container>
     </>
   );
